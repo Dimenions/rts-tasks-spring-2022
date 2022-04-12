3da683313b5b75c4162e37edf7391a1fc24868b5
Dmitriy <dimenions@mail.ru>
2022-04-12 15:56:58 +0400

    Add Task

--- task01/task01.ino
+++ task01/task01.ino
@@ -1,7 +1,46 @@
-void setup() {
+//Вариант 400 мс 150 мс  100 мс  250 мкс 35 мкс
 
+struct diode
+{
+  int pin;
+  int status;
+  unsigned long previousTime;
+  unsigned long interval;
+};
+
+const int diodeCount = 5;
+struct diode diodes[] = {
+  2, LOW, 0, 400000,
+  3, LOW, 0, 150000,
+  4, LOW, 0, 100000,
+  5, LOW, 0, 250,
+  6, LOW, 0, 35
+};
+
+
+void setup() {
+  for (int a = 0; a < diodeCount; a++)
+  {
+    pinMode(diodes[a].pin, OUTPUT);
+  }
 }
 
-void loop() {
+void loop()
+{
+  unsigned long time = micros();
 
+  for (int a = 0; a < diodeCount; a++)
+  {
+    if (time - diodes[a].previousTime > diodes[a].interval)
+    {
+      diodes[a].previousTime = time;
+
+      if (diodes[a].status == LOW)
+        diodes[a].status = HIGH;
+      else
+        diodes[a].status = LOW;
+
+      digitalWrite(diodes[a].pin, diodes[a].status);
+    }
+  }
 }
--- task02/task02.ino
+++ task02/task02.ino
@@ -1,7 +1,54 @@
+//Вариант 400 мс 150 мс  100 мс  250 мкс 35 мкс
+
+#include <TaskManagerIO.h>
+
+bool pin5State = false;
+bool pin6State = false;
+bool pin9State = false;
+bool pin10State = false;
+bool pin11State = false;
+
+
+void checkState2() {
+    digitalWrite(2, pin2State);
+    pin2State = !pin2State;           
+}
+
+void checkState3() {
+    digitalWrite(3, pin3State);
+    pin3State = !pin3State;           
+}
+
+void checkState4() {
+    digitalWrite(4, pin4State);
+    pin4State = !pin4State;           
+}
+
+void checkState5() {
+    digitalWrite(5, pin5State);
+    pin5State = !pin5State;           
+}
+
+void checkState6() {
+    digitalWrite(6, pin6State);
+    pin6State = !pin6State;           
+}
+
 void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  
+  taskManager.scheduleFixedRate(400000, checkState2, TIME_MICROS);
+  taskManager.scheduleFixedRate(150000, checkState3, TIME_MICROS);
+  taskManager.scheduleFixedRate(100000, checkState4, TIME_MICROS);
+  taskManager.scheduleFixedRate(250, checkState5, TIME_MICROS);
+  taskManager.scheduleFixedRate(35, checkState6, TIME_MICROS);
 
 }
 
 void loop() {
-
+  taskManager.runLoop();
 }
